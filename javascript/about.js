8e22bc37463081807b983d2f0409d2de5ec8c05c
Simedru Florin <simedruflorin@yahoo.com>
2024-12-20 18:49:32 +0200

    comments in english

--- javascript/about.js
+++ javascript/about.js
@@ -1,19 +1,19 @@
-// Selectăm elementele
+// Select the elements
 const menuButton=document.getElementById('menuButton');
 const modal=document.getElementById('modalabout');
 const closeButton=document.getElementById('closeButton');
 
-// Afișăm modalul când se face click pe buton
+// Show modal window click pe buton
 menuButton.addEventListener('click', ()=> {
         modal.style.display='flex';
     });
 
-// Închidem modalul când se apasă pe butonul de închidere
+// Close modal window when close button is clicked  
 closeButton.addEventListener('click', ()=> {
         modal.style.display='none';
     });
 
-// Închidem modalul când se face click în afara conținutului
+// Close modal window when click outside the content
 window.addEventListener('click', (event)=> {
         if (event.target===modal) {
             modal.style.display='none';
--- javascript/login.js
+++ javascript/login.js
@@ -1,19 +1,19 @@
-// Selectăm elementele
+// Select the elements
 const loginButton = document.getElementById('loginButton');
 const modal = document.getElementById('loginModal');
 const closeButton=document.getElementById('closeButton');
 
-// Afișăm modalul când se face click pe buton
+// Show modal window when button is clicked
 loginButton.addEventListener('click', ()=> {
         modal.style.display='flex';
     });
 
-// Închidem modalul când se apasă pe butonul de închidere
+// Close modal window when close button is clicked
 closeButton.addEventListener('click', ()=> {
         modal.style.display='none';
     });
 
-// Închidem modalul când se face click în afara conținutului
+// Close modal window when click outside the content
 window.addEventListener('click', (event)=> {
         if (event.target===modal) {
             modal.style.display='none';
