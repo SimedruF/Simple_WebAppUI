a7990c69d61daa4b616ed99b3ddf54cb14fb2196
SimedruF <simedruflorin@yahoo.com>
2025-04-28 21:28:22 +0300

    fix

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -5,36 +5,35 @@ dropdowns.forEach((dropdown) => {
     const dropdownButton = dropdown.querySelector('.dropdown-menu-button');
     const dropdownContent = dropdown.querySelector('.dropdown-content');
     let timeout;
+
     // Verificăm dacă dropdownButton și dropdownContent există
     if (!dropdownButton || !dropdownContent) {
         console.warn('Dropdown-ul nu are un buton sau conținut valid:', dropdown);
         return; // Ieșim din funcție pentru acest dropdown
     }
+
     // Adăugăm eveniment de click pentru a activa/dezactiva dropdown-ul
     dropdownButton.addEventListener('click', (event) => {
         event.stopPropagation(); // Previne propagarea evenimentului către window
         closeAllDropdowns(); // Închide toate celelalte dropdown-uri
-        dropdown.classList.toggle('active');
-        dropdownContent.style.display = dropdown.classList.contains('active') ? 'block' : 'none';
+        dropdownContent.classList.toggle('active');
     });
 
     // Închidem dropdown-ul dacă se face click în afara lui
     window.addEventListener('click', () => {
-        dropdown.classList.remove('active');
-        dropdownContent.style.display = 'none';
+        dropdownContent.classList.remove('active');
     });
 
     // Adăugăm evenimente pentru a adăuga un delay la închiderea meniului dropdown
     dropdown.addEventListener('mouseleave', () => {
         timeout = setTimeout(() => {
-            dropdownContent.style.display = 'none';
-            dropdown.classList.remove('active');
+            dropdownContent.classList.remove('active');
         }, 50); // Delay de 50ms înainte de a închide meniul
     });
 
     dropdown.addEventListener('mouseenter', () => {
         clearTimeout(timeout);
-        dropdownContent.style.display = 'block';
+        dropdownContent.classList.add('active');
     });
 });
 
@@ -42,7 +41,6 @@ dropdowns.forEach((dropdown) => {
 function closeAllDropdowns() {
     dropdowns.forEach((dropdown) => {
         const dropdownContent = dropdown.querySelector('.dropdown-content');
-        dropdown.classList.remove('active');
-        dropdownContent.style.display = 'none';
+        dropdownContent.classList.remove('active');
     });
 }
